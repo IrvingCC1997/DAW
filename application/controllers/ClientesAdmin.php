9a54d1c55ddbac4a24e2ec7f00b9f3ed8e5de4e1
albertoleonm <albertoleonm21@gmail.com>
2019-06-21 18:01:49 -0500

    Agregamos modulo y mejoramos el diseño y funcionalidad del sitioi

---
+++ application/controllers/ClientesAdmin.php
@@ -0,0 +1,34 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class ClientesAdmin extends CI_Controller {
+
+	// Función que ejecuta el constructor
+	public function __construct(){
+		parent::__construct();
+
+		// Cargamos la librería de Grosery Crud
+		$this->load->library('grocery_CRUD');
+	}
+
+	public function index(){
+		// Se utiliza un try porque estamos trabajando con bases de datos.
+		try{
+			$crud = new grocery_CRUD();
+			// EL nombre del tema que se va a utilizar
+			$crud->set_theme('bootstrap-v4');
+			$crud->set_table('clientes');
+			// Cambia el nombre de la table (Alias)
+			$crud->set_subject('Clientes');
+			$crud->columns('nombreCliente', 'apellidoCliente', 'correoElectronico', 'contrasena');
+			$crud->required_fields('nombreCliente', 'apellidoCliente', 'correoElectronico', 'contrasena');
+
+			$output = $crud->render();
+			// Es un casteo por eso se antepone la palabra array
+			$this->load->view('Admin/clientesA.php', (array)$output);
+		 }catch(Exception $e){
+			 // Si hay un error se muestra en pantalla
+			 show_error($e->getMessage());
+		 }
+	}
+}
