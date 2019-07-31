c39a703968e8f545c8c22d60ac8bb7575e590d9e
albertoleonm <albertoleonm21@gmail.com>
2019-07-31 13:47:19 -0500

    Se modificaron las imagenes que usabamos en el slider por unas propias además de modificar el style.css para que se adaptaran

--- application/views/inicio.php
+++ application/views/inicio.php
@@ -16,41 +16,38 @@
 						<div class="carousel-item active">
 						<div class="view">
 							<img class="image1 images-slider" src="">
-							<div class="mask rgba-black-light"></div>
+							<div class="mask"></div>
 						</div>
 						<div class="carousel-caption">
-							<h3 class="h3-responsive">Tractores de calidad.</h3>
 						</div>
 						</div>
 						<div class="carousel-item">
 						<!--Mask color-->
 						<div class="view">
 							<img class="image2 images-slider" src="">
-							<div class="mask rgba-black-light"></div>
+							<div class="mask "></div>
 						</div>
 						<div class="carousel-caption">
-							<h3 class="h3-responsive">Insumos agrícolas de calidad.</h3>
 						</div>
 						</div>
 						<div class="carousel-item">
 						<!--Mask color-->
 						<div class="view">
 							<img class="image3 images-slider" src="">
-							<div class="mask rgba-black-light"></div>
+							<div class="mask"></div>
 						</div>
 						<div class="carousel-caption">
-							<h3 class="h3-responsive">Refacciones.</h3>
 						</div>
 						</div>
 					</div>
 					<!--/.Slides-->
 					<!--Controls-->
 					<a class="carousel-control-prev" href="#carousel-example-2" role="button" data-slide="prev">
-						<span class="carousel-control-prev-icon" aria-hidden="true"><i class="fas fa-arrow-left"></i></span>
+						<span class="carousel-control-prev-icon" aria-hidden="true"><i class="fas fa-arrow-left arrow"></i></span>
 						<span class="sr-only">Previous</span>
 					</a>
 					<a class="carousel-control-next" href="#carousel-example-2" role="button" data-slide="next">
-						<span class="carousel-control-next-icon" aria-hidden="true"><i class="fas fa-arrow-right"></i></span>
+						<span class="carousel-control-next-icon" aria-hidden="true"><i class="fas fa-arrow-right arrow"></i></span>
 						<span class="sr-only">Next</span>
 					</a>
 					<!--/.Controls-->
