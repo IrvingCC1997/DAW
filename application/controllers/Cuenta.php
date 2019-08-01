b38def9f87cbada9b3abfbb23d9912c8e0539cb9
Alberto Leon <albertoleonm21@gmail.com>
2019-08-01 17:25:45 -0500

    Se inicia la sesión después de que el usuario se registra

--- application/controllers/Cuenta.php
+++ application/controllers/Cuenta.php
@@ -82,10 +82,49 @@ class Cuenta extends CI_Controller {
 		$this->Cuenta_model->set_nombreCliente($this->input->post("nombre"));
         $this->Cuenta_model->set_apellidoCliente($this->input->post("apellido"));
 		$this->Cuenta_model->set_correoCliente($this->input->post("mail"));
+		
 		// Pasamos la contraseña cifrada al modelo
 		$this->Cuenta_model->set_contrasena($ciphertext);
-        $this->Cuenta_model->registrar_Usuario();
-        redirect('Inicio');
+
+		// Aqui se registra el usuario
+		$sesion = $this->Cuenta_model->registrar_Usuario();
+		
+		// Hacemos la consulta al modelo para ver si ya se registro
+		$dataLog = $this->Cuenta_model->log();
+
+		// Revisamos si el usuario existe
+		if($dataLog != null){
+			// Como retorna un array lo tenemos que recorrer
+			foreach($dataLog as $aux){
+				// Lo que regrese en la variable contrasena lo guardamos en otra variable
+				$password = $aux->contrasena;
+				$name = $aux->nombreCliente;
+				$mail = $aux->correoElectronico;
+			}
+
+			// Desencriptamos ambas contraseñas y comparamos si son similares las contraseñas
+			if($this->encryption->decrypt($ciphertext) == $this->encryption->decrypt($password)){
+				
+				// Asignamos valores a un array para utilizar con las sesiones 
+				$dataLogin = array(
+					'correElectronico' => $mail,
+					'nombre' => $name,
+					'login' => true
+				);
+
+				// Pasamos el array con la información de recuperada en la consulta
+				$this->session->set_userdata($dataLogin);
+				redirect('Inicio');
+			}else{ // La contraseña es correcta
+				echo "Contraseña incorrecta";
+				die();
+				redirect('Cuenta');
+			}
+		}else{ // El usuario no existe
+			echo "No existe el usuario";
+			die();
+			redirect('Cuenta');
+		}
 	}
 
 	public function logout(){
