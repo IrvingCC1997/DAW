4ef7a05f1e56714150607840306aea33b7fafee9
Alberto Leon <albertoleonm21@gmail.com>
2019-08-01 16:59:43 -0500

    Cambié de windows a linux y modifique el archivo

--- application/config/database.php
+++ application/config/database.php
@@ -75,9 +75,9 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
+	'hostname' => '127.0.0.1',
+	'username' => 'akephalos',
+	'password' => '1234',
 	'database' => 'agrotracsem',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
